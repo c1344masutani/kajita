ad679417dd453015145985e5d313e7c9a7843ae1
Masutani Akihiro <c1344@oic.jp>
2022-10-20 11:32:24 +0900

    aaa

--- kajita/Form1.Designer.cs
+++ kajita/Form1.Designer.cs
@@ -34,9 +34,9 @@ namespace kajita
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(212, 114);
+            this.button1.Location = new System.Drawing.Point(12, 99);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 215);
+            this.button1.Size = new System.Drawing.Size(745, 215);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
